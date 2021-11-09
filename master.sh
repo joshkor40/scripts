3f4bdccabb41c080cd1c01285e5625e65da00603
joshkor40 <30927040+joshkor40@users.noreply.github.com>
2021-11-09 11:54:40 -0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -23,10 +23,6 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 EOF
 sleep_and_clear
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-
 echo "${cy}RESETING AND ENABLING DOCKER${rst}"
     echo
     sudo systemctl enable docker >/dev/null
